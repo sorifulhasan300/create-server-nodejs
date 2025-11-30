20598a39a38d693445396d2de3d9e9648fe7f016
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-11-30 21:51:59 +0600

    handle route with helper Route Handler

---
+++ src/helper/RouteHandler.ts
@@ -0,0 +1,11 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+export type RouteHandler = (res: IncomingMessage, req: ServerResponse) => void;
+
+export const routes: Map<string, Map<string, RouteHandler>> = new Map();
+
+function addRoutes(method: string, path: string, handler: RouteHandler) {
+  if (!routes.has(method)) routes.set(method, new Map());
+  routes.get(method)!.set(path, handler);
+}
+export default addRoutes;
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,49 @@
 import http, { IncomingMessage, ServerResponse } from "http";
 import config from "./config";
+import addRoutes, { RouteHandler, routes } from "./helper/RouteHandler";
+
+addRoutes("GET", "/", (req, res) => {
+  res.end(
+    JSON.stringify({
+      Massage: "Hello from node js and typescript",
+      path: req.url,
+    })
+  );
+});
 const server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("Server is Running......");
 
-    if (req.url == "/" && req.method == "GET") {
-      res.writeHead(200, { "content-type": "application/json" });
+    const method = req.method?.toUpperCase() || "";
+    const path = req.url || "";
+    const methodMap = routes.get(method);
+    const handler: RouteHandler | undefined = methodMap?.get(path);
+    if (handler) {
+      handler(req, res);
+    } else {
+      res.writeHead(400, { "content-type": "Application/json" });
       res.end(
-        JSON.stringify({
-          Massage: "Hello from node js and typescript..",
-          path: req.url,
-        })
+        JSON.stringify({ success: false, message: "route not found", path })
       );
     }
+    // if (req.url == "/user" && req.method == "POST") {
+    //   let body = "";
+    //   req.on("data", (chunk) => {
+    //     body += chunk.toString();
+    //   });
 
-    if (req.url == "/user" && req.method == "POST") {
-      let body = "";
-      req.on("data", (chunk) => {
-        body += chunk.toString();
-      });
-
-      req.on("end", () => {
-        const data = JSON.parse(body);
-        console.log("received data", data);
-        res.writeHead(200, { "content-type": "application/json" });
-        res.end(
-          JSON.stringify({
-            Massage: "this is user data",
-            received: data,
-          })
-        );
-      });
-    }
+    //   req.on("end", () => {
+    //     const data = JSON.parse(body);
+    //     console.log("received data", data);
+    //     res.writeHead(200, { "content-type": "application/json" });
+    //     res.end(
+    //       JSON.stringify({
+    //         Massage: "this is user data",
+    //         received: data,
+    //       })
+    //     );
+    //   });
+    // }
   }
 );
 server.listen(config.port, () => {
